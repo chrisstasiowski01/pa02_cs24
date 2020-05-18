6051c160ae334630bb78f90eaee477c441518c7a
Chris Stasiowski <cstasiowski@umail.ucsb.edu>
2020-05-17 17:52:46 -0700

    push7

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@
 #include <cstring>
 #include <algorithm>
 
+#include "movies.h"
 using namespace std;
-
-bool parseLine(string &line, string &movieName, double &movieRating);
 
 int main(int argc, char** argv){
   if(argc < 4){
@@ -27,8 +26,6 @@ int main(int argc, char** argv){
   }
   
   ifstream movieFile (argv[2]);
-  string line, movieName;
-  double movieRating;
 
   if (movieFile.fail()){
     cerr << "Could not open file " << argv[2];
@@ -37,36 +34,14 @@ int main(int argc, char** argv){
 
   // Create an objects of the BST class you defined 
   // to contain the name and rating in the input file
-
+  Movies tree;
   // Read each file and store the name and rating
-  while (getline (movieFile, line) && parseLine(line, movieName, movieRating)){
-    // Use std::string movieName and double movieRating
-    // to construct your Movie objects
-    cout << movieName << " has rating " << movieRating << endl;
-  }
+  tree.createTree(argv[2]);
+  tree.printPreODepth();
+  cout << endl;
+  tree.highestRating(argv[3]);
   movieFile.close();
 
   return 0;
-}
-
-bool parseLine(string &line, string &movieName, double &movieRating) {
-  if(line.length() <= 0) return false;
-  string tempRating = "";
-
-  bool flag = false;
-  movieName = tempRating = "", movieRating = 0.0, flag = false;
-
-  for (int i = 0; i < line.length(); i++){
-    if(flag) tempRating += line[i];
-    else if(line[i]==','&& line[0]!='"') flag = true;
-    else {
-      if(i==0 && line[0]=='"') continue;
-      if(line[i]=='"'){ i++; flag=true; continue;}
-      movieName += line[i];
-    }
-  }
-  
-  movieRating = stod(tempRating);
-  return true;
 }
 
